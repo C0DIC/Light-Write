f94c2f665d993bc56161df80f2d9e302a1b35103
Moonwalker1996 <michaellivesforever23@gmail.com>
2020-05-25 23:46:44 +0400

    Update2.9.5

---
+++ src/lwapp.py
@@ -0,0 +1,107 @@
+import os
+from tkinter import * 
+from tkinter import scrolledtext, filedialog
+
+def save_file(event):
+	try:
+		files = filedialog.asksaveasfile(mode = 'w', 
+		filetypes = (('Python', '*.py'), ('Text', '*.txt'),('Ruby', '*.rb'), ('Nim', '*.nim'), ('HTML', '*.html'),
+		('C##', '*.cs'), ('C++', '*.cpp'), ('Rust', '*.rs'), ('CSS', '*.css'),
+		('GO', '*.go'), ('PHP', '*.php'),('Java', '*.java'), ('JavaScript', '*.js'), ('TypeScript', '*.ts'),
+		('Scala', '*.sc')))
+		app.title("Light Write: " + str(files.name))
+		data = write.get('1.0', END)
+		files.write(data)
+	except AttributeError:
+		pass
+
+def open_file(event):
+	try:
+		files = filedialog.askopenfile(mode = 'r')
+		app.title("Light Write: " + str(files.name))
+		data = files.read()
+		write.delete('1.0', END)
+		write.insert('1.0', data)
+	except (UnicodeDecodeError, AttributeError):
+		comm_line.insert(2, "It's not a text file")
+
+def new_file(event):
+	app.title("Light Write - [UNSAVED]")
+	write.delete('1.0', END)
+
+def black(event):
+		app.config(bg = 'black')
+		write.config(bg = 'black', fg = 'white', highlightbackground = 'black', insertbackground = 'white')
+		comm_line.config(bg = 'black', fg = 'white', highlightbackground = 'black', insertbackground = 'white')
+
+def white(event):
+	app.config(bg = 'white')
+	write.config(bg = 'white', fg = 'black', highlightbackground = 'white', insertbackground = 'black')
+	comm_line.config(bg = 'white', fg = 'black', highlightbackground = 'white', insertbackground = 'black')
+
+def sky(event):
+	app.config(bg = '#2a2d4a')
+	write.config(bg = '#2a2d4a', fg = 'white', highlightbackground = '#2a2d4a', insertbackground = 'white')
+	comm_line.config(bg = '#2a2d4a', fg = 'white', highlightbackground = '#2a2d4a', insertbackground = 'white')
+
+def cold(event):
+	app.config(bg = "#afc2cc")
+	write.config(bg = "#afc2cc", fg = "#1b647a", highlightbackground = "#1b647a", insertbackground = "#1b647a")
+	comm_line.config(bg = "#afc2cc", fg = "#1b647a", highlightbackground = "#1b647a", insertbackground = "#1b647a")
+
+def size(event):
+	try:
+		comm = comm_line.get()
+		if int(comm) > 9 and int(comm) < 18:
+			write.config(font = ('Sans', int(comm)))
+		else:
+			comm_line.insert(3, " | Only from 10 to 17")
+	except ValueError:
+		pass
+
+def makeAppDir(event):
+	try:
+		os.mkdir(os.path.expanduser("~/LWFiles"))
+	except OSError:
+		comm_line.insert(3, " | Failed creating directory")
+	else:
+		comm_line.insert(3, " | Successfully created app directory")
+
+def focusing_d(event):
+	comm_line.focus()
+
+def focusing_u(event):
+	write.focus()
+
+def esc(event):
+	app.destroy()
+
+app = Tk()
+app.title('Light Write')
+app.config(bg = 'white')
+app.minsize(width = '660', height = '500')
+app.maxsize(height = '800')
+
+app.bind('<Control-s>', save_file)
+app.bind('<Control-o>', open_file)
+app.bind('<Control-n>', new_file)
+
+app.bind('<Control-b>', black)
+app.bind('<Control-k>', sky)
+app.bind('<Control-w>', white)
+app.bind('<Control-l>', cold)
+
+app.bind('<Control-Shift-A>', makeAppDir)
+app.bind('<Control-d>', size)
+app.bind('<Control-Up>', focusing_u)
+app.bind('<Control-Down>', focusing_d)
+app.bind('<Escape>', esc)
+
+write = Text(app, bg = 'white', bd = 1)
+write.pack(expand = True, fill = BOTH)
+write.focus()
+
+comm_line = Entry(app, bg = 'white', bd = 1)
+comm_line.pack(side = RIGHT, fill = BOTH, expand = True)
+
+app.mainloop()
